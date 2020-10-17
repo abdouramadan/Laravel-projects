4c8f8b29c00bf7a0911e9eb80659a1d31c746d37
abdouraamadan <abdouraamadan36@gmail.com>
2020-10-17 11:05:57 +0300

    Added laravel project

---
+++ blogOne/resources/views/home/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.layout')
+@section('title') Home @endsection
+
+@section('content')
+    <header class="mainHeader">
+        <nav class="mainNavbar navbar navbar-expand-lg navbar-transparent">
+            <div class="container">
+                <a href="" class="navbar-brrand text-capitalize">abdev</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-capitalize">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-capitalize">about</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-capitalize">blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-capitalize">contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+@endsection
